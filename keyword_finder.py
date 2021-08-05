b38afb24efd093756a0a29c27f94acb7cb95958e
Prajwal S <58431432+prajwalsvclk@users.noreply.github.com>
2021-08-05 21:53:13 +0530

    Update keyword_finder.py

--- keyword_finder.py
+++ keyword_finder.py
@@ -6,7 +6,7 @@ def main():
  "except", "False", "finally", "for", "from", "global", "if", "import", "in", "is", 
 "lambda", \ 
  "None", "nonlocal", "not", "or", "pass", "raise", "return", "True", "try", "while", 
-"with", "yield"} 
+"with", "yield"} #user given keywords or you can import some keywords library
  filename = input("Enter a Python source code filename: ").strip() 
  if not os.path.isfile(filename): # Check if file exists 
  print("File", filename, "does not exist") 
